9ef418136c04424e0ac7f1148b5f4f406860099e
Evan Pavlica <evan@pavlica.us>
2023-10-20 13:58:34 -0700

    Remove CDK bootstrap version from cfn templates

--- bin/integration-templates.ts
+++ bin/integration-templates.ts
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import { App, DefaultStackSynthesizer } from 'aws-cdk-lib';
 import { LambdaToEventbridgeStack } from '../lib/lambda-to-eventbridge-stack';
 import { StepfunctionsToEventbridgeStack } from '../lib/stepfunctions-to-eventbridge-stack';
 import { EventbridgeToEventbridgeStack } from '../lib/eventbridge-to-eventbridge-stack'
 
-const app = new cdk.App();
-new LambdaToEventbridgeStack(app, 'LambdaToEventbridgeStack', {});
-new StepfunctionsToEventbridgeStack(app, 'StepfunctionsToEventbridgeStack', {});
-new EventbridgeToEventbridgeStack(app, 'EventbridgeToEventbridgeStack', {});
+const app = new App();
+const defaultOpts = {
+  synthesizer: new DefaultStackSynthesizer({
+    generateBootstrapVersionRule: false,
+  })
+};
+new LambdaToEventbridgeStack(app, 'LambdaToEventbridgeStack', defaultOpts);
+new StepfunctionsToEventbridgeStack(app, 'StepfunctionsToEventbridgeStack',defaultOpts);
+new EventbridgeToEventbridgeStack(app, 'EventbridgeToEventbridgeStack',defaultOpts);
